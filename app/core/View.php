6cf71c1f5511fc4073a802d795a41cbc554e1149
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-12-03 22:34:26 +0200

    add View

---
+++ app/core/View.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\core;
+class View
+{
+    const VIEWS_DIR = '..' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR;
+
+    public $template = 'default';
+
+    public function __construct(string $template = null)
+    {
+        if($template){
+            $this->template = $template;
+        }
+    }
+
+    /**
+     * Show front
+     * @param string $page
+     * @param array $data
+     * @return void
+     */
+    public function render(string $page,array $data) : void
+    {
+        extract($data);
+        include_once self::VIEWS_DIR . 'templates' . DIRECTORY_SEPARATOR . $this->template . '.php';
+    }
+}
